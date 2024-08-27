72c0c0d2645767f15c3a704b1495c9ffb7330ae3
K9-Thoai <thoaiaust@gmail.com>
2024-08-27 10:26:24 +0700

    sua bai

--- lesson-2/javascript/ex4.js
+++ lesson-2/javascript/ex4.js
@@ -2,11 +2,9 @@ let chieucao = 181
 chia = chieucao%100
 
 cannanglt = (chia*9)/10
-console.log("Cân nặng lí tưởng: ", cannanglt);
-
 cannangtd = chia
-console.log("Cân nặng tối đa: ", cannangtd);
-
 cannangtt = (chia*8)/10
-console.log("Cân nặng tối thiểu: ", cannangtt);
+
+console.log("Cân nặng lí tưởng: ", cannanglt, ", Cân nặng tối đa: ", cannangtd, ", Cân nặng tối thiểu: ", cannangtt);
+
 
