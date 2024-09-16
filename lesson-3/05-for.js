c8c98752664f815a88b8c94ecb9e9add1de8eb24
K9-Thoai <thoaiaust@gmail.com>
2024-09-16 13:53:23 +0700

    Upload 05-for.js

---
+++ lesson-3/05-for.js
@@ -0,0 +1,28 @@
+// 1
+let student = {"name": "Thoại", "age": 22};
+//  console.log("name="+student.name , "\nage="+student.age);
+console.log("1:");
+
+ for(let stu in student){
+    console.log(` ${stu}=${student[stu]}` );
+}
+
+// 2
+let student2 = {"name": "Thoại", "age": 22, "salary": 20};
+let tong = 0;
+for(let stu in student2){
+    if(student2[stu] > 0){
+        tong = tong + student2[stu];
+    }
+}
+console.log(tong);
+
+// 3
+let student1 = {"name": "Thoại", "age": 22};
+arr = [];
+
+for(let stu in student){
+    arr.push(`${stu}`);
+}
+console.log("3:");
+console.log(arr);
