46e68536adcaf69b22df720af3517c973a883997
K9-Thoai <thoaiaust@gmail.com>
2024-09-17 22:35:52 +0700

    Update 04-foreach.js

--- lesson-3/04-foreach.js
+++ lesson-3/04-foreach.js
@@ -17,42 +17,51 @@ arr1 = [1, 2, 3, 4];
 console.log("2:");
 
 // Sử dụng for
-function tinhTong(arr1){
-    let tong = 0;
-    for(let i=0; i<arr1.length; i++){
-        tong = tong + arr1[i];
-    }
-    return tong;
-}
-console.log(tinhTong(arr1));
+// function tinhTong(arr1){
+//     let tong = 0;
+//     for(let i=0; i<arr1.length; i++){
+//         tong = tong + arr1[i];
+//     }
+//     return tong;
+// }
+// console.log(tinhTong(arr1));
 
-let max = arr1[0];
-let min = arr1[0];
-for(let i=0; i<arr1.length; i++){
-    if(arr1[i]>max){
-        max = arr1[i];
-    }
-    if(arr1[i]<min){
-        min = arr1[i];
-    }
-}
-console.log(`Số Lớn Nhất Là: ${max}`);
-console.log(`Số Nhỏ Nhất Là: ${min}`);
+// let max = arr1[0];
+// let min = arr1[0];
+// for(let i=0; i<arr1.length; i++){
+//     if(arr1[i]>max){
+//         max = arr1[i];
+//     }
+//     if(arr1[i]<min){
+//         min = arr1[i];
+//     }
+// }
+// console.log(`Số Lớn Nhất Là: ${max}`);
+// console.log(`Số Nhỏ Nhất Là: ${min}`);
 
 // Sử dụng forEach
-// arr1.forEach(function(n){
-//     console.log(n + n);
-// })
+let sum = 0;
+let max = arr1[0];
+let min = arr1[0];
 
-// arr1.forEach((so, index) => {
-//     if(so.arr1> arr1[i]){
-//         console.log(so);
-//     }
-// })
+arr1.forEach(item => {
+    sum = sum + item;
+
+    if(item > max){
+        max = item;
+    }
+
+    if(item < min){
+        min = item;
+    }
+});
+
+console.log(`Tổng: ${sum}, Max: ${max}, Min: ${min}`);
 
 // 3
 arr2 = [2, 3, 4];
 arr3 = [];
+console.log("3:");
 
 // Sử dụng For
 // function gapDoi(arr2){
@@ -61,14 +70,12 @@ arr3 = [];
 //         arr3.push(themMoi);
 //     }
 // }
-// console.log("3:");
 // gapDoi(arr2)
 // console.log(arr3);
 
 // Sử dụng forEach
-arr2.forEach(function(n){
-    so = n*2;
-    arr3.push(so);
+arr2.forEach(n => {
+    arr3.push(n*2);
 })
 console.log(arr3);
 
