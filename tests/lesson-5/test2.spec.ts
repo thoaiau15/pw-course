6d9fdf6461de94bdb140ce69e283d79a7e80a26e
K9-Thoai <thoaiaust@gmail.com>
2024-09-25 08:52:30 +0700

    feat: Update test1, test2

--- tests/lesson-5/test2.spec.ts
+++ tests/lesson-5/test2.spec.ts
@@ -1,24 +1,34 @@
 import {test, expect} from '@playwright/test';
 
-test('Test 2', async ({ page }) => {
-    await page.goto("https://material.playwrightvn.com/");
-    await page.locator('//a[@href="02-xpath-product-page.html"]').click();
+test('Bài 2: Product page', async ({ page }) => {
+    // Đi đến materia.playwrightvn.com
+    await test.step('Đi đến trang chủ material', async() => {
+        await page.goto("https://material.playwrightvn.com/");
+    })
 
-    await page.locator('//button[@class="add-to-cart" and @data-product-id="1"]').click({
-        clickCount: 2
-    });
-    await page.locator('//button[@class="add-to-cart" and @data-product-id="2"]').click({
-        clickCount: 3
-    });
-    await page.locator('//button[@class="add-to-cart" and @data-product-id="3"]').click({
-        clickCount: 1
-    });
+    // Chọn vào Bài học 2: Product page
+    await test.step('Đi đến trang Bài học 2: Product page', async() => {
+        await page.locator('//a[@href="02-xpath-product-page.html"]').click();
+    })
 
-
-    await expect(page.locator("//tbody//tr")).toHaveCount(3);
-    await expect(page.locator("//tbody/tr[1]/td").nth(2)).toHaveText("2");
-    await expect(page.locator("//tbody/tr[2]/td").nth(2)).toHaveText("3");
-    await expect(page.locator("//tbody/tr[3]/td").nth(2)).toHaveText("1");
-
-
+    // Thực hiện click vào các sản phẩm theo yêu cầu
+    await test.step('Thực hiện thêm các sản phẩm', async () => {
+        await page.locator('//button[@class="add-to-cart" and @data-product-id="1"]').click({
+            clickCount: 2
+        });
+        await page.locator('//button[@class="add-to-cart" and @data-product-id="2"]').click({
+            clickCount: 3
+        });
+        await page.locator('//button[@class="add-to-cart" and @data-product-id="3"]').click({
+            clickCount: 1
+        });
+    })
+    
+    // Phần test thêm - Kiểm tra xem đã hiển thị trong giỏ hàng hay chưa
+    await test.step('Thực hiện kiểm tra xem test trên vào giỏ hàng thành công', async () => {
+        await expect(page.locator("//tbody//tr")).toHaveCount(3);
+        await expect(page.locator("//tbody/tr[1]/td").nth(2)).toHaveText("2");
+        await expect(page.locator("//tbody/tr[2]/td").nth(2)).toHaveText("3");
+        await expect(page.locator("//tbody/tr[3]/td").nth(2)).toHaveText("1");
+    })
 });
