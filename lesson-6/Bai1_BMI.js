1c5184a69ee0d34a55b19a27bd0ad570918e841a
K9-Thoai <thoaiaust@gmail.com>
2024-09-27 15:45:49 +0700

    feat: Upload lesson6

---
+++ lesson-6/Bai1_BMI.js
@@ -0,0 +1,16 @@
+function BMI(c, n) {
+    let bmi = n / (c * c);
+    if (bmi < 18.5) {
+        console.log("Thiếu Cân");
+    }
+    else if (bmi < 25) {
+        console.log("Bình Thường");
+    }
+    else if (bmi < 30) {
+        console.log("Thừa Cân");
+    }
+    else if (bmi >= 30) {
+        console.log("Béo Phì");
+    }
+};
+BMI(1.81, 75);
